9fa3ad70d95ff65ba0077a97e786735eb7a37881
ravi <ravi.gulia@sportswizzleague.com>
2021-11-23 15:44:04 +0530

    fillmcq

--- src/routes.php
+++ src/routes.php
@@ -7,13 +7,13 @@ use Illuminate\Support\Facades\Route;
 
 // Route::get('test', 'EdgeWizz\Fillup\Controllers\FillupController@test')->name('test');
 
-Route::post('fmt/Fillmcq/store', 'EdgeWizz\Fillmcq\Controllers\FillmcqController@store')->name('fmt.Fillmcq.store');
+Route::post('fmt/Fillmcq/store', 'EdgeWizz\Fillmcq\Controllers\FillmcqController@store')->name('fmt.fillmcq.store');
 
-Route::post('fmt/Fillmcq/update/{id}', 'EdgeWizz\Fillmcq\Controllers\FillmcqController@update')->name('fmt.Fillmcq.update');
+Route::post('fmt/Fillmcq/update/{id}', 'EdgeWizz\Fillmcq\Controllers\FillmcqController@update')->name('fmt.fillmcq.update');
 
-Route::post('fmt/Fillmcq/csv_upload', 'EdgeWizz\Fillmcq\Controllers\FillmcqController@uploadFile')->name('fmt.Fillmcq.csv');
+Route::post('fmt/Fillmcq/csv_upload', 'EdgeWizz\Fillmcq\Controllers\FillmcqController@uploadFile')->name('fmt.fillmcq.csv');
 
-Route::any('fmt/Fillmcq/delete/{id}',  'EdgeWizz\Fillmcq\Controllers\FillmcqController@delete')->name('fmt.Fillmcq.delete');
+Route::any('fmt/Fillmcq/delete/{id}',  'EdgeWizz\Fillmcq\Controllers\FillmcqController@delete')->name('fmt.fillmcq.delete');
 
-Route::any('fmt/Fillmcq/inactive/{id}',  'EdgeWizz\Fillmcq\Controllers\FillmcqController@inactive')->name('fmt.Fillmcq.inactive');
-Route::any('fmt/Fillmcq/active/{id}',  'EdgeWizz\Fillmcq\Controllers\FillmcqController@active')->name('fmt.Fillmcq.active');
+Route::any('fmt/Fillmcq/inactive/{id}',  'EdgeWizz\Fillmcq\Controllers\FillmcqController@inactive')->name('fmt.fillmcq.inactive');
+Route::any('fmt/Fillmcq/active/{id}',  'EdgeWizz\Fillmcq\Controllers\FillmcqController@active')->name('fmt.fillmcq.active');
